ed042fbcd39e172b970ce356ab98ea9056d4cb08
Serhii Smirnov <sergej.smirnov.sss@gmail.com>
2020-02-16 14:24:40 +0200

    first task

--- Smirnov_Lab5/main.py
+++ Smirnov_Lab5/main.py
@@ -1,80 +1,116 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import math
 from copy import deepcopy
 
 
-alpha_root = np.array([[0.6, 0.5, 0.4, 0.55, None, 0.755, 0.45],
-                       [None, None, 0.7, 0.35, None, None, 0.7],
-                       [0.65, None, 0.8, 0.65, 0.7, 0.65, 0.7],
-                       [None, None, 0.4, 0.55, 0.45, 0.85, None]])
+alpha_root = np.array([[0.2, None, 0.3, 0.45, 0.7, 0.3, 0.5],
+                       [None, 0.4, 0.3, None, 0.6, 0.5, None],
+                       [None, 0.35, 0.4, None, 0.4, 0.7, None],
+                       [0.3, 0.25, 0.4, 0.55, 0.5, None, None]])
 
 
-Ip_root = np.array([[0.65, 0.55, 0.8, 0.45, None, 0.7, 0.75],
-                   [None, None, 0.5, 0.8, None, None, 0.45],
-                   [0.45, None, 0.6, 0.5, 0.6, 0.45, 0.45],
-                   [None, None, 0.7, 0.7, 0.4, 0.35, None]])
+Ip_root = np.array([[0.4, None, 0.5, 0.5, 0.8, 0.4, 0.6],
+                   [None, 0.6, 0.5, None, 0.3, 0.6, None],
+                   [None, 0.4, 0.4, None, 0.3, 0.75, None],
+                   [0.5, 0.4, 0.4, 0.65, 0.3, None, None]])
 
 
-Id_root = np.array([[0.3, 0.54, 0.5, 0.4, None, 0.5, 0.4],
-                   [None, None, 0.65, 0.25, None, None, 0.5],
-                   [0.35, None, 0.35, 0.4, 0.2, 0.45, 0.3],
-                   [None, None, 0.65, 0.5, 0.65, 0.3, None]])
+Id_root = np.array([[0.6, None, 0.7, 0.4, 0.7, 0.45, 0.6],
+                   [None, 0.5, 0.75, None, 0.4, 0.3, None],
+                   [None, 0.3, 0.55, None, 0.45, 0.8, None],
+                   [0.7, 0.3, 0.65, 0.4, 0.65, None, None]])
 
 
-It_root = np.array([[0.7, 0.8, 0.4, 0.6, None, 0.85, 0.5],
-                    [None, None, 0.5, 0.4, None, None, 0.4],
-                    [0.25, None, 0.76, 0.45, 0.3, 0.5, 0.6],
-                    [None, None, 0.5, 0.5, 0.6, 0.3, None]])
+It_root = np.array([[0.4, None, 0.5, 0.5, 0.8, 0.5, 0.8],
+                    [None, 0.5, 0.6, None, 0.6, 0.4, None],
+                    [None, 0.6, 0.7, None, 0.4, 0.3, None],
+                    [0.5, 0.7, 0.8, 0.6, 0.2, None, None]])
 
 
-def get_Ip(t, gamma, alpha, Ip_prev):
-    temp = 1 + 0.25 * Ip_prev * (1 + 0.5 * alpha) * (1 + gamma) * t * (1 + 0.5 * alpha) * (1 + gamma) * t
+def get_Ip(t, alpha, Ip_prev):
+    temp = Ip_prev * (1 + 0.5 * (1 + alpha) * t)
     if 0 < temp < 1:
         return temp
     return 1
 
 
-def get_Id(t, alpha_prev, Id_prev, Ip_prev):
-    temp = 1 + 0.25 * (1 + Id_prev) * (1 + alpha_prev * t) * (1 + alpha_prev * t) / ((1 + 10 * Ip_prev) * (1 + 10 * Ip_prev))
+def get_Id(t, gamma, Id_prev):
+    temp = Id_prev * (1 + (1 + gamma) * t)
     if 0 < temp < 1:
         return temp
     return 1
 
-def get_It(t, beta, It_prev, T1, T2):
-    temp = 1 - 0.25 * It_prev * (1 + (1 + beta * t) * (1 + beta * t))
-    if 0 < temp < 1 and T1 <= t <= T2:
+
+def get_It(t, gamma, It_prev, T1, T2):
+    temp = It_prev * (1 - gamma * t)
+    if 0 < temp <= 1 and T1 <= t <= T2:
         return temp
     return 0
 
 
-def get_alpha(alpha_prev, It_prev, Ip_prev):
+def get_alpha(alpha_prev, Ip_prev):
     if 0 < alpha_prev < 1:
-        return 1 + 0.5 * (It_prev + Ip_prev) * alpha_prev
+        return alpha_prev * Ip_prev / (1 + alpha_prev)
     return 0
 
 
-def get_beta(alpha_prev, It_prev):
+def get_beta(alpha_prev, Id_prev, It_prev):
     if 0 < alpha_prev < 1:
-        return 1 + 0.5 * (1 + 0.01 * alpha_prev) * (1 + 0.01 * alpha_prev) / (It_prev * It_prev * (1 + alpha_prev))
+        return 0.01 * It_prev / (1 + alpha_prev * Id_prev)
     return 0
 
 
-def get_gamma(alpha_prev, Id_prev, It_prev):
+def get_gamma(alpha_prev, beta, Id_prev):
     if 0 < alpha_prev < 1:
-        return (1 + alpha_prev * alpha_prev * Id_prev) / (1 + It_prev)
+        return (1 + alpha_prev * Id_prev) * (1 + alpha_prev * Id_prev) / (100 * beta + 5 * (1 + alpha_prev * alpha_prev * alpha_prev))
     return 0
 
 
 def get_entropy(t, alpha, beta, gamma, Ip, Id, It):
-    return 1 - math.log2(1 + alpha * It * Ip * Id * (1 + alpha * t) * (1 + gamma * t) * (1 - beta * t * t))
+    return 1 - math.log2(1 + It * Ip * Id * (1 + alpha * t) * (1 + gamma * t) * (1 + beta * t) * (7 + gamma))
+
+
+def get_one_history(data=None, name=None, index_i=0, index_j=0):
+    li = []
+    for index_time in range(len(data)):
+        li.append((data.loc[index_time]).I[name][index_i, index_j])
+    return li
+
+
+def graph_plot(dataset, I_name, index_i, index_j, x_label='time, s'):
+    fix, ax = plt.subplots()
+    ax.plot(t, get_one_history(dataset, I_name, index_i, index_j))
+    ax.set(xlabel=x_label, ylabel=I_name, title='Physics of {}[{}, {}]'.format(I_name, index_i, index_j))
+    ax.grid()
+    fix.savefig("Physics of_{}[{}, {}].png".format(I_name, index_i, index_j))
+    return fix.show()
+
+
+def init_decision_time(example):
+    temp = deepcopy(example)
+    for i in range(len(temp)):
+        for j in range(len(temp[0])):
+            if temp[i, j] is not None:
+                temp[i, j] = [0, 'infinity']
+    return temp
+
+def check_infinity(value):
+    try:
+        int(value)
+        return True
+    except:
+        return False
 
 
 if __name__ == "__main__":
     eta = 0.6  # user should write this value on UI
 
+    #initialize necessary tensors
     df_history = pd.DataFrame(columns=['t', 'I'])
-    t = [i for i in range(0, 10)]
+    decision_time = init_decision_time(Ip_root)
+    t = [i/10 for i in range(0, 20)]
 
     for i in t:
         df_history.loc[len(df_history)] = [i, {'Ip': deepcopy(Ip_root), 'Id': deepcopy(Id_root), 'It': deepcopy(It_root)}]
@@ -84,22 +120,26 @@ if __name__ == "__main__":
 
                 if alpha_root[j, k] is not None:
 
-                    alpha = get_alpha(alpha_root[j, k], It_root[j, k], Ip_root[j, k])
-                    beta = get_beta(alpha_root[j, k], It_root[j, k])
-                    gamma = get_gamma(alpha_root[j, k], Id_root[j, k], It_root[j, k])
-                    Ip = get_Ip(i, gamma, alpha, Ip_root[j, k])
-                    Id = get_Id(i, alpha_root[j, k], Id_root[j, k], Ip_root[j, k])
-                    It = get_It(i, beta, It_root[j, k], T1=0, T2=1000)
+                    alpha = get_alpha(alpha_root[j, k], Ip_root[j, k])
+                    beta = get_beta(alpha_root[j, k], Id_root[j, k], It_root[j, k])
+                    gamma = get_gamma(alpha_root[j, k], beta, Id_root[j, k])
+                    Ip = get_Ip(i, alpha, Ip_root[j, k])
+                    Id = get_Id(i, gamma, Id_root[j, k])
+                    T2 = decision_time[j, k][1] if check_infinity(decision_time[j, k][1]) else max(t)
+                    It = get_It(i, gamma, It_root[j, k], T1=min(t), T2=T2)
 
                     entropy = get_entropy(i, alpha, beta, gamma, Ip, Id, It)
-                    print("t={}, entropy={}".format(i, entropy))
-                    if 0 <= entropy <= eta:
-                        hahah = 0
 
-                    # rewrite previous alpha
+                    if entropy > eta and check_infinity(decision_time[j, k][1]) == False:
+                        decision_time[j, k] = [0, i]
+
+                    # rewrite previous
                     alpha_root[j, k] = alpha
                     Ip_root[j, k] = Ip
                     Id_root[j, k] = Id
                     It_root[j, k] = It
 
-    #print((df_history.loc[5]).I['It'])
+    #graph_plot(df_history, 'Ip', 0, 0)
+    #graph_plot(df_history, 'Id', 0, 0)
+    #graph_plot(df_history, 'It', 0, 0)
+    #print(decision_time)
